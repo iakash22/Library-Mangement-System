4f1ef0cbe75693713d1b8014046f91e6d8801363
Akash kumar <94979388+iakash22@users.noreply.github.com>
2023-05-08 13:59:36 +0530

    compeleted

---
+++ main.cpp
@@ -0,0 +1,65 @@
+#include<iostream>
+using namespace std;
+class Library{
+    public :
+    int id;
+    char name[100];
+    char author[100];
+    char student[100];
+    int price;
+    int pages;
+};
+int main(){
+    Library lib[20];
+    int input=0;
+    int count = 0;
+
+    while(input != 3){
+        cout<<"Enter 1 to input details like ->\n : id \n : name \n : author \n : student \n : price \n : pages"<<endl;
+        cout<<"Enter 2 to display details"<<endl;
+        cout<<"Enter 3 to Exit."<<endl;
+        cin>>input;
+
+        switch(input){
+            case 1: 
+            start:
+            cout<<"Enter Book Id"<<endl;
+            cin>>lib[count].id;
+            cout<<"Enter Book Name"<<endl;
+            // cin>>lib[count].name;
+            cin.getline(lib[count].name,100,'.');
+            cout<<"Enter Book Author"<<endl;
+            // cin>>lib[count].author;
+            cin.getline(lib[count].author,100,'.');
+            cout<<"Enter Student Name"<<endl;
+            // cin>>lib[count].student;
+            cin.getline(lib[count].student,100,'.');
+            cout<<"Enter Book Price"<<endl;
+            cin>>lib[count].price;
+            cout<<"Enter Book Pages"<<endl;
+            cin>>lib[count].pages;
+            count++;
+            break;
+
+            case 2:
+            for(int i=0; i<count; i++){
+                cout<<"Book Id : "<<lib[i].id<<endl;
+                cout<<"Book name : "<<lib[i].name;
+                cout<<"Book Author : "<<lib[i].author;
+                cout<<"Student Name : "<<lib[i].student;
+                cout<<"Book Price : "<<lib[i].price<<endl;
+                cout<<"Book pages : "<<lib[i].pages<<endl;
+            }
+            break;
+
+            case 3:
+            exit(0);
+            break;
+
+            default:
+            cout<<"Eror!!!!!!!!! \n Enter Valid Key "<<endl;
+            goto start;
+        }
+    }
+    return 0;
+}
